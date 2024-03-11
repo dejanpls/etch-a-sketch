9e7ab3783af7ed5f48aca7d7319100c0585760be
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-03-11 08:33:15 +0100

    Generate a custom grid
    What was done:
    	- When 'generate-grid' button is pressed generate
    	  1x1 up to 100x100 grid, based on user's input
    	- Add the event listener to 'generate-grid' button

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ let currentColor = "black";
 
 generateStartGrid();
 
+// Event listeners
+generateBtn.addEventListener("mousedown", generateNewGrid);
+
 // Functions
 function generateStartGrid() {
 	const rows = 16;
@@ -37,4 +40,28 @@ function generateGrid(rows, columns, numberOfBoxes) {
 		gridBox.style.height = `${600 / columns}px`
 		gridContainer.appendChild(gridBox);
 	}
+}
+
+function generateNewGrid() {
+	const rows = generateRows();
+	const columns = rows;
+	let numberOfBoxes = rows * columns;
+
+	// If existing grid, remove it.
+	existingGridBoxes = document.querySelectorAll("div.grid-box");
+	if (existingGridBoxes.length > 0) {
+		existingGridBoxes.forEach(box => gridContainer.removeChild(box));
+	}
+
+	generateGrid(rows, columns, numberOfBoxes);
+}
+
+function generateRows() {
+	let rows;
+
+	do {
+		rows = parseInt(prompt("Enter a number between 1 and 100: "));
+	} while (Number.isNaN(rows) || rows > 100 || rows < 1)
+
+	return rows
 }
